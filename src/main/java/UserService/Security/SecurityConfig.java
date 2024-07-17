d8b7f0011e532a65904c35ae5333419493ba7e41
mariog44 <mg441236@gmail.com>
2024-07-17 18:05:34 -0500

    CLASE DE SEGURIDAD. Se agregó la clase de seguridad para tener un encriptador de constraseñas.

---
+++ src/main/java/UserService/Security/SecurityConfig.java
@@ -0,0 +1,15 @@
+package UserService.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    PasswordEncoder Encryptor() {
+        return new BCryptPasswordEncoder();
+    }
+}
